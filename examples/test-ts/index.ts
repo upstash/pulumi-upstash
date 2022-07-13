b5f30a8a58dba6b6e38d850a241aedf5f3801a7e
burak-upstash <burak@upstash.com>
2022-07-13 23:25:24 +0300

    add ts examples

--- examples/test-ts/index.ts
+++ examples/test-ts/index.ts
@@ -8,12 +8,61 @@ const createdDb = new upstash.RedisDatabase("mydb", {
     tls: true
 })
 
-
-
 const dbFromGet = upstash.getRedisDatabaseOutput({
     databaseId: createdDb.databaseId
+})
+
+
+
+const createdCluster = new upstash.KafkaCluster("myCluster", {
+    clusterName: "pulumi-ts-cluster",
+    region: "eu-west-1",
+})
+
+const clusterFromGet = upstash.getKafkaClusterOutput({
+    clusterId: createdCluster.clusterId
+})
+
+
+
+const createdTopic = new upstash.KafkaTopic("myTopic", {
+    clusterId: clusterFromGet.clusterId,
+    cleanupPolicy: "delete",
+    maxMessageSize: 100,
+    partitions: 1,
+    retentionSize: 1000,
+    retentionTime: 1000,
+    topicName: "pulumi-ts-topic"
+
+})
+
+const topicFromGet = upstash.getKafkaTopicOutput({
+    topicId: createdTopic.topicId
+})
+
+
+
+const createdTeam = new upstash.Team("myTeam", {
+    teamName: "pulumi ts team",
+    teamMembers: {
+        "bylmaz744@gmail.com": "owner",
+        "burak@upstash.com": "admin"
+    },
+    copyCc : true
+})
+
+const teamFromGet = upstash.getTeamOutput({
+    teamId: createdTeam.teamId
 })
 
 export const db = createdDb
 export const dbFromGetResult = dbFromGet
 
+export const cluster = createdCluster
+export const clusterFromGetResult = clusterFromGet
+
+export const topic = createdTopic
+export const topicFromGetResult = topicFromGet
+
+export const team = createdTeam
+export const teamFromGetResult = teamFromGet
